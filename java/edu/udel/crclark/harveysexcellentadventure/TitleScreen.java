a8a2d568681071f1562bf0e6cb8cf68edc4f97fc
Collin Clark <crclark96@gmail.com>
2016-05-08 20:17:21 -0400

    init

---
+++ java/edu/udel/crclark/harveysexcellentadventure/TitleScreen.java
@@ -0,0 +1,13 @@
+package edu.udel.crclark.harveysexcellentadventure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
