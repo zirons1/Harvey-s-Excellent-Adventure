7c7a8f2660f679a51051b2ce87dbef01cb3c2a31
Collin Clark <crclark96@gmail.com>
2016-05-12 17:29:24 -0400

    Added buttons to game over screen

--- java/edu/udel/crclark/harveysexcellentadventure/GameOver.java
+++ java/edu/udel/crclark/harveysexcellentadventure/GameOver.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class GameOver extends AppCompatActivity {
     TextView t;
     int score;
-    Intent i;
+    Intent i, i2;
     Bundle extras;
     String newScore;
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,21 @@ public class GameOver extends AppCompatActivity {
             if (newScore != null && t != null)
                 t.setText(newScore);
         }
+
+        b1 = (Button) findViewById(R.id.button5);
+        b2 = (Button) findViewById(R.id.button6);
     }
     @Override
     protected void onResume(){
         super.onResume();
+    }
+
+    public void onClickRetry(View v){
+        i2 = new Intent(this, Ep1MainActivity.class);
+        startActivity(i2);
+    }
+    public void onClickMainMenu(View v){
+        i2 = new Intent(this, MainMenu.class);
+        startActivity(i2);
     }
 }
